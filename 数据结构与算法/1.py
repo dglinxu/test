06f810ff0e1e152d860792065a63e8c515505f63
虎门资源 <humen@189.cn>
2019-06-06 11:50:05 +0800

    6-6

--- 数据结构与算法/1.py
+++ 数据结构与算法/1.py
@@ -1,4 +1,7 @@
-h=[1,2,5,2,6,1]
-ls=sorted(h)
-print(len(h))
-print(ls)
+nums=[0,0,0,1,2,2,5,6,7,8,8]
+l=len(nums)
+t=[nums[0]]
+for i in range(1,l):
+    if nums[i-1]!=nums[i]:
+        t.append(nums[i])
+print(t)
