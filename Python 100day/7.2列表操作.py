c06ce5d823f3f91d18e8e28a6a318d644b223415
dglinxu <humen@189.cn>
2019-07-10 20:48:40 +0800

    day7

--- Python 100day/7.2列表操作.py
+++ Python 100day/7.2列表操作.py
@@ -1,18 +1,16 @@
+import os
+import time
+
+
 def main():
-    list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
-    list2 = sorted(list1)
-    # sorted函数返回列表排序后的拷贝不会修改传入的列表
-    # 函数的设计就应该像sorted函数一样尽可能不产生副作用
-    list3 = sorted(list1, reverse=True)
-    # 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
-    list4 = sorted(list1, key=len)
-    print(list1)
-    print(list2)
-    print(list3)
-    print(list4)
-    # 给列表对象发出排序消息直接在列表对象上进行排序
-    list1.sort(reverse=True)
-    print(list1)
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')  # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
 
 
 if __name__ == '__main__':
