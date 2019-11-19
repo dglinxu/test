cd0d92df3b7a2e5700404c0c0fd2fd82169d5737
虎门资源 <humen@189.cn>
2019-11-19 10:57:33 +0800

    1

--- 小董Python/1.py
+++ 小董Python/1.py
@@ -5,9 +5,16 @@
 # s=set(li)
 # for i in s:
 #     print('%d数字的次数是：%d'%(i,li.count(i)),end='\n')
-import time 
-for i in range(200): 
-    print(str(i))
-    # print(len(i),end='\r')
-    time.sleep(0.1).
-    # print(' '*len(str(i)),end='\r')
+import time
+li = ["\\", "|", "/", "—"] #第一个两个斜杠是为了转义
+
+for i in range(50):
+    # \r 表示将光标的位置回退到本行的开头位置
+    # \b表示将光标的位置回退一位
+    # print('\r%d'%i,end='') #光标在字符后面
+    # print(i,end='\r') #光标在字符前面
+    # print('\r'+'*'*i,end=' ')
+    # j = i % 4
+    # print('\r'+li[j], end='')
+    print('\r'+'※'*i,end='') #进度条
+    time.sleep(0.2)
