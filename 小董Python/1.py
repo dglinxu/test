6be8805b6824f95a08fb3a5abe9ca6272f6b99f0
虎门资源 <humen@189.cn>
2019-11-19 10:07:13 +0800

    1

--- 小董Python/1.py
+++ 小董Python/1.py
@@ -1,2 +1,13 @@
-a=["flower","flow","flight"]
-print(min(len(i) for i in a))
+# from random import randint
+# a=["flower","flow","flight"]
+# print(min(len(i) for i in a))
+# li=[randint(1,10) for i in range(25)]
+# s=set(li)
+# for i in s:
+#     print('%d数字的次数是：%d'%(i,li.count(i)),end='\n')
+import time 
+for i in range(200): 
+    print(str(i))
+    # print(len(i),end='\r')
+    time.sleep(0.1).
+    # print(' '*len(str(i)),end='\r')
