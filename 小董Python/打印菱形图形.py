3e39c3e4226d508b9a5c05887070006fb8c85d91
虎门资源 <humen@189.cn>
2019-11-25 16:16:01 +0800

    1

--- 小董Python/打印菱形图形.py
+++ 小董Python/打印菱形图形.py
@@ -1,12 +1,12 @@
 def p_picture(n):
-    p=
     h=n*2-1
-    for i in range(h):
-        if i<n:
-            s='*'+' *'*i
-        else:
-            s='*'+' *'*(h-i)
-        print(' '*(abs(n-i))+s)
+    for i in range(n*2-1):
+        s='*'+' *'*(i if i<n else 2*n-2-i)
+        # else: 
+        #     s='*'+' *'*(2*n-2-i)
+        
+        print(' '*(abs(n-i-1))+s)
 
 
-p_picture(6)
+
+p_picture(7)
