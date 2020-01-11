1ebfe374abb14517760a199a574c30054de83e17
dglinxu <humen@189.cn>
2020-01-11 18:11:57 +0800

    111

--- 待装测试/Pandas VS Excel/生成电子表格.py
+++ 待装测试/Pandas VS Excel/生成电子表格.py
@@ -2,32 +2,53 @@ import pandas as pd
 import datetime
 import random
 
-def addMonth(date,am):
-    y=am//12
-    m=date.month+am%12
-    if m>12:
-        y=y+m//12
-        m=m%12
-    return datetime.date(date.year+y,m,date.day)
-'''如果在读取时设置了index,那么不能对这一列进行修改，写入时也不会自动增加一列索引'''
-stu=pd.read_excel('output1.xlsx',skiprows=3,usecols='d:h',index_col='Id',
-                  dtype={'Id':str,'学号':str,'成绩':str,'等级':str,'日期':str})
-startdate=datetime.date(2020,1,1)
-for i in stu.index:
-    # stu['Id'].at[i]=i+1
-    stu['成绩'].at[i]=random.randrange(70,100)
-    stu['等级'].at[i]='Good' if stu['成绩'].at[i]>85 else 'Bad'
-    # stu['日期'].at[i]=datetime.timedelta(days=i)+startdate   #按日增加
-    # stu['日期'].at[i] =datetime.date(startdate.year+1,startdate.month,startdate.day) #按年增加
-    stu['日期'].at[i] = addMonth(startdate,i) #按月增加
-print(stu)
-# stu=stu.set_index('Id')
 
-stu.to_excel('output2.xlsx')
+'''筛选'''
+# price=pd.read_excel('output2.xlsx',index_col='Id')
+# price=price.loc[price['价格'].apply(lambda x:110<x<120)].loc[price.喜好.apply(lambda x:x=='Y')]
+# # price=price.loc[price.价格.apply(lambda x:110<x)]
+# print(price)
+
+''' 排序  '''
+# price=pd.read_excel('output2.xlsx',index_col='Id')
+# price.sort_values(by=['喜好','价格'],inplace=True,ascending=[True,False])
+# print(price)
+''' 运算  '''
+# price=pd.read_excel('output2.xlsx',index_col='Id')
+# price['价格']=price['价格'].apply(lambda x:x+10)
+# price['售价']=price['价格']*price['折扣']
+# price['商品']=price['商品']+'--折扣商品'
+# price.to_excel('output2.xlsx')
+
+''' 填充  '''
+# def addMonth(date,am):
+#     y=am//12
+#     m=date.month+am%12
+#     if m>12:
+#         y=y+m//12
+#         m=m%12
+#     return datetime.date(date.year+y,m,date.day)
+# """如果在读取时设置了index,那么不能对这一列进行修改，写入时也不会自动增加一列索引"""
+# stu=pd.read_excel('output1.xlsx',skiprows=3,usecols='d:h',index_col='Id',
+#                   dtype={'Id':str,'学号':str,'成绩':str,'等级':str,'日期':str})
+# startdate=datetime.date(2020,1,1)
+# for i in stu.index:
+#     # stu['Id'].at[i]=i+1
+#     stu.at[i,'成绩']=random.randrange(70,100) #有两种赋值形式，注意顺序
+#     stu['等级'].at[i]='Good' if stu['成绩'].at[i]>85 else 'Bad'
+#     # stu['日期'].at[i]=datetime.timedelta(days=i)+startdate   #按日增加
+#     # stu['日期'].at[i] =datetime.date(startdate.year+1,startdate.month,startdate.day) #按年增加
+#     stu['日期'].at[i] = addMonth(startdate,i) #按月增加
+# print(stu)
+# # stu=stu.set_index('Id')
+#
+
+''' 创建表格  '''
+# stu.to_excel('output2.xlsx')
 # df=pd.DataFrame({'Id':[1,2,3],'Name':['阿土仔','孙小美','钱夫人']})
 # df=df.set_index('Id')
 # df.to_excel('pandas创建的表格.xlsx')
-
+''' 读取表格  '''
 # dz=pd.read_excel("test.xlsx",index_col='Id')
 # dz.to_excel('output.xlsx')
 # print(dz.shape)
@@ -35,7 +56,7 @@ stu.to_excel('output2.xlsx')
 # print(dz.head())
 # print('-'*50)
 # print(dz.tail(20))
-
+''' 由序列生成表格  '''
 # s1=pd.Series([1,2,3],index=[1,2,3],name='A')
 # s2=pd.Series([10,20,30],index=[1,2,3],name='B')
 # s3=pd.Series([100,200,300],index=[1,2,3],name='C')
