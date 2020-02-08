ec8f3f7ab0a15348d76f6a1f78d723d459353a81
dglinxu <humen@189.cn>
2020-02-08 10:46:34 +0800

    1

--- SQL练习/t.py
+++ SQL练习/t.py
@@ -2,7 +2,7 @@ import sqlite3
 conn=sqlite3.connect('测试数据库.db')
 c=conn.cursor()
 
-def create_table():
-    sql='CREATE TABLE IF NOT EXISTS students(rollno int,name text,class int) '
+def create_table(): 
+    sql='CREATE TABLE IF NOT EXISTS student11s(rollno int,name text,class int) '
     c.execute(sql)
 create_table()
