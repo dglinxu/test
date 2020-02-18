c47c635e5ec1a99dd7002ca96fd17d00d6d34db4
虎门资源 <humen@189.cn>
2020-02-18 09:33:17 +0800

    1

--- python基础/数组全展开.py
+++ python基础/数组全展开.py
@@ -1,12 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""""""""""""""""""""""""""
-# @版      本：V1.0
-# @作      者：资源
-# @联系方式：humen@189.cn
-# @创建软件：PyCharm
-# @项目名称：Git
-# @文  件  名：数组全展开.py
-# @修改时间：2020/1/14 14:56
-# @文件说明：
-""""""""""""""""""""""""""
+from collections.abc import *
+
+def flatten(lst, out_lst=None):
+    if out_lst is None:
+        out_lst = []
+    for i in lst:
+        if isinstance(i, Iterable): # 判断i是否可迭代
+            flatten(i, out_lst)  # 尾数递归
+        else:
+            out_lst.append(i)    # 产生结果
+    return out_lst
+print(flatten([[1,2,3],[4,5]]))
+print(flatten([[1,2,3],[4,5]], [6,7]))
+print(flatten([[[1,2,3],[4,5,6]]]))
+
+def filter_false(lst):
+    return list(filter(bool, lst))
+
+
+r = filter_false([None, 0, False, '', [], 'ok', [1, 2]])
+print(r)  # ['ok', [1, 2]]
+
